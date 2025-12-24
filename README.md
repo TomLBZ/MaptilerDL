a63bf7fe18ef9af75bda71387f90a0d5cbc99071
TomLBZ <libozhao142857@hotmail.com>
2025-12-24 03:11:42 +0000

    added config file and examples for sg, and modified args for better usability

--- README.md
+++ README.md
@@ -9,12 +9,13 @@ This script allows you to download map tiles from the MapTiler API for offline u
 | `--dir` / `-d`   | The directory where the downloaded tiles will be saved. | `./tiles` | No |
 | `--type` / `-t`  | The type of tiles to download,<br> (e.g., `sat`, `cnt`, `trgb`). | `sat` | No |
 | `--zoom` / `-z`  | The zoom level of the tiles to download. | `0` | No |
-| `--range` / `-r` | The bounding box for tiles,<br> specified as `minlon minlat maxlon maxlat`. | `-180 -90 180 90` | No |
+| `--bounds` / `-b` | The bounding box for tiles,<br> specified as `minlon minlat maxlon maxlat`. | `-180 -90 180 90` | No |
+| `--config` / `-c` | Path to a [configuration file](#configuration-file) for batch download of different zoom / bounding box settings. | `""` | No |
 | `--help` / `-h`   | Show help message and exit. | N/A | N/A |
 
 To download a specific zoom level of tiles from MapTiler API, you can use the following command:
 ```bash
-python3 tiledl.py -k <API_KEY> -d <dir> -t <type> -z <zoom> -r <range>
+python3 tiledl.py -k <API_KEY> -d <dir> -t <type> -z <zoom> -b <bounds>
 ```
 ### Supported Tile Types
 | Tile Type | Description | Alias | Extension |
@@ -26,6 +27,31 @@ python3 tiledl.py -k <API_KEY> -d <dir> -t <type> -z <zoom> -r <range>
 | `v4` | MapTiler Planet v4 tiles | `v4`, `v4tiles`, `v4-tiles`, `tilesv4`, `tiles-v4` | `pbf` |
 | `landform` | Landform tiles | `landform`, `lf`, `landforms`, `lfs` | `pbf` |
 
+### Configuration File
+You can create a configuration file to specify multiple zoom levels and bounding boxes for batch downloading. The configuration file should be in comma-separated `csv` format with headers `zoom,minlon,minlat,maxlon,maxlat`.
+- **Edit in Excel**
+    | zoom | minlon | minlat | maxlon | maxlat |
+    |------|--------|--------|--------|--------|
+    | 0    | -180   | -90    | 180    | 90    |
+    | ...  | ...    | ...    | ...    | ...    |
+- **Edit in Text Editor**
+    ```csv
+    zoom,minlon,minlat,maxlon,maxlat
+    0,-180,-90,180,90
+    ...,...,...,...,...
+    ```
+Some example configuration files are provided in this repository for downloading tiles for Singapore. They are:
+- [sg_sat.csv](./configs/sg_sat.csv): Configuration file for downloading `satellite` tiles for Singapore.
+- [sg_cnt.csv](./configs/sg_cnt.csv): Configuration file for downloading `contour` tiles for Singapore.
+- [sg_trgb.csv](./configs/sg_trgb.csv): Configuration file for downloading `terrain-rgb` tiles for Singapore.
+- [sg_vx.csv](./configs/sg_vx.csv): Configuration file for downloading `v3` or `v4` tiles for Singapore.
+- [sg_lf.csv](./configs/sg_lf.csv): Configuration file for downloading `landform` tiles for Singapore.
+
+#### *Important Notes*
+- When using a configuration file, the `--zoom` and `--bounds` arguments will be ignored.
+- When creating a configuration file, ***only*** include the zoom levels and bounding boxes that are ***available for the selected tile type***. Some tile types may not have data for all zoom levels or regions.
+- Make sure header names are correct and there are no extra spaces.
+
 ### Examples
 1. To download all satellite tiles at zoom level 2 at the `./tiles` folder, you would run:
     ```bash
@@ -33,7 +59,11 @@ python3 tiledl.py -k <API_KEY> -d <dir> -t <type> -z <zoom> -r <range>
     ```
 2. To download contour tiles at zoom level 5 for a specific bounding box (`-10 -10 10 10`) and save them in a custom directory(`./contour_tiles`), you would run:
     ```bash
-    python3 tiledl.py -k <API_KEY> -d ./contour_tiles -t cnt -z 5 -r -10 -10 10 10
+    python3 tiledl.py -k <API_KEY> -d ./contour_tiles -t cnt -z 5 -b -10 -10 10 10
+    ```
+3. To download landform tiles at zoom level 8 using the provided config file ([sg_lf.csv](./configs/sg_lf.csv)) and save them in a custom directory(`~/tiles/landform`), you would run:
+    ```bash
+    python3 tiledl.py -k <API_KEY> -d ~/tiles/landform -t lf -c ./configs/sg_lf.csv
     ```
 ## Download Tile Data for Singapore
 To download tiles for a region, at different zoom levels different bounding boxes should be used. Here are example bounding boxes for different zoom levels in Singapore:
@@ -43,25 +73,30 @@ To download tiles for a region, at different zoom levels different bounding boxe
 - For zoom levels `13` to `15`, the tiles should be downloaded based on the specified bounding box `103.5 0.9 104.2 1.6`.
 - For zoom levels `16` onwards (if applicable), the tiles should be downloaded based on the specified bounding box `103.6920359 1.1304753 104.0120359 1.4504753`.
 
-Example command to download the `v4` tiles for Singapore at different zoom levels under the `~/tiles/v4` directory:
+Example command to download the `sat` tiles for Singapore using the provided config file ([sg_sat.csv](./configs/sg_sat.csv)) and save them in the `~/tiles/sat` directory:
+```bash
+python3 tiledl.py -k <API_KEY> -d ~/tiles/sat -t sat -c ./configs/sg_sat.csv
+```
+
+Example command to download the `v4` tiles manually without using a configuration file for Singapore at different zoom levels under the `~/tiles/v4` directory:
 ```bash
 # for zoom levels 0 to 6
 python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 0
 # repeat the above command changing zoom level from 1 to 6
 
 # for zoom levels 7 to 9
-python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 7 -r 80 -20 120 20
+python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 7 -b 80 -20 120 20
 # repeat the above command changing zoom level from 7 to 9
 
 # for zoom levels 10 to 12
-python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 10 -r 102 -1 106 3
+python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 10 -b 102 -1 106 3
 # repeat the above command changing zoom level from 10 to 12
 
 # for zoom levels 13 to 15
-python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 13 -r 103.5 0.9 104.2 1.6
+python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 13 -b 103.5 0.9 104.2 1.6
 # repeat the above command changing zoom level from 13 to 15
 
 # for zoom level 16 onwards
-python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 16 -r 103.6920359 1.1304753 104.0120359 1.4504753
+python3 tiledl.py -k <API_KEY> -d ~/tiles/v4 -t v4 -z 16 -b 103.6920359 1.1304753 104.0120359 1.4504753
 # repeat the above command changing zoom level from 16 onwards
 ```
--- tiledl.py
+++ tiledl.py
@@ -7,19 +7,24 @@ from typing import Optional, Tuple, List
 from dataclasses import dataclass
 
 # types, classes and data structures:
-type TileRange = Tuple[float, float, float, float] # (minlon, minlat, maxlon, maxlat)
+type TileBounds = Tuple[float, float, float, float] # (minlon, minlat, maxlon, maxlat)
 @dataclass(frozen=True, slots=True, kw_only=True)
 class TileOption:
     name: str
     ext: str
     aliases: List[str]
 @dataclass(frozen=True, slots=True, kw_only=True)
+class LevelConfig:
+    zoom: int
+    bounds: TileBounds
+@dataclass(frozen=True, slots=True, kw_only=True)
 class TileDLArguments:
     key: str
     dir: str
     option: TileOption
     zoom: int
-    range: TileRange
+    bounds: TileBounds
+    config: str = ""
 @dataclass(frozen=True, slots=True, kw_only=True)
 class BackoffConfig:
     initial_wait: float = 1.0
@@ -33,13 +38,14 @@ class BackoffConfig:
 class GlobalVariables:
     wait_sec: float = 1.0
     downloaded_count: int = 0
+    total_downloaded_count: int = 0
 
 # constants:
 MIN_LON: float = -179.99999999
 MAX_LON: float = 179.99999999
 MIN_LAT: float = -85.0511
 MAX_LAT: float = 85.0511
-MAX_RANGE: TileRange = (MIN_LON, MIN_LAT, MAX_LON, MAX_LAT)
+MAX_BOUNDS: TileBounds = (MIN_LON, MIN_LAT, MAX_LON, MAX_LAT)
 URL_TEMPLATE: str = "https://api.maptiler.com/tiles/{t}/{z}/{x}/{y}.{e}?key={k}"
 TILE_OPTIONS: List[TileOption] = [
     TileOption(name="satellite-v2", ext="jpg", aliases=["satellite", "satellite-v2", "satellitev2", "sat"]),
@@ -55,40 +61,76 @@ def parse_arguments() -> TileDLArguments:
     parser = argparse.ArgumentParser(description="Download tiles from MapTiler.")
     # required arguments: key
     parser.add_argument("-k", "--key", type=str, required=True, default="get_your_own_OpIi9ZULNHzrESv6T2vL", help="Your MapTiler API key.")
-    # optional arguments: dir, type, zoom, range
+    # optional arguments: dir, type, zoom, bounds, config
     parser.add_argument("-d", "--dir", type=str, default="./tiles", help="Directory to save tiles")
     parser.add_argument("-t", "--type", type=str, choices=TYPE_CHOICES, metavar="TYPE", 
                         default="sat", help="Map Type (sat, v4, etc.)")
     parser.add_argument("-z", "--zoom", type=int, choices=range(0, 23), metavar="ZOOM", 
                         default=0, help="Zoom level (0-22), recommended 0-15")
-    parser.add_argument("-r", "--range", type=float, nargs=4, metavar=("MINLON", "MINLAT", "MAXLON", "MAXLAT"), 
-                        default=MAX_RANGE, help="Bounding box to download tiles")
+    parser.add_argument("-b", "--bounds", type=float, nargs=4, metavar=("MINLON", "MINLAT", "MAXLON", "MAXLAT"), 
+                        default=MAX_BOUNDS, help="Bounding box to download tiles")
+    parser.add_argument("-c", "--config", type=str, default="", help="Path to configuration file")
     args = parser.parse_args()
     return TileDLArguments(
         key=args.key,
         dir=args.dir,
         option=next((option for option in TILE_OPTIONS if args.type in option.aliases), TILE_OPTIONS[0]),
         zoom=args.zoom,
-        range=tuple(args.range)
+        bounds=tuple(args.bounds),
+        config=args.config
     )
-def get_response_dynamic_backoff(gvar: GlobalVariables, cfg: BackoffConfig, url: str) -> Optional[requests.Response]:
-    for _ in range(cfg.max_retries):
+def load_config(path: str) -> List[LevelConfig]:
+    file_content: str # file content is a csv with headers: zoom,minlon,minlat,maxlon,maxlat
+    try:
+        with open(path, "r") as f:
+            file_content = f.read()
+    except Exception as e:
+        print(f"Error reading configuration file: {e}")
+        return []
+    lines = file_content.strip().splitlines()
+    if len(lines) < 2:
+        print("Configuration file is empty or has no data.")
+        return []
+    headers = lines[0].split(",")
+    expected_headers = ["zoom", "minlon", "minlat", "maxlon", "maxlat"]
+    if [h.strip().lower() for h in headers] != expected_headers:
+        print("Configuration file has invalid headers.")
+        return []
+    level_configs: List[LevelConfig] = []
+    for line in lines[1:]:
+        parts = line.split(",")
+        if len(parts) != 5:
+            print(f"Invalid line in configuration file: {line}")
+            continue
         try:
-            response = requests.get(url, timeout=cfg.timeout)
+            zoom = int(parts[0].strip())
+            minlon = float(parts[1].strip())
+            minlat = float(parts[2].strip())
+            maxlon = float(parts[3].strip())
+            maxlat = float(parts[4].strip())
+            level_configs.append(LevelConfig(zoom=zoom, bounds=(minlon, minlat, maxlon, maxlat)))
+        except ValueError as e:
+            print(f"Error parsing line in configuration file: {line}. Error: {e}")
+            continue
+    return level_configs
+def get_response_dynamic_backoff(gvar: GlobalVariables, bcfg: BackoffConfig, url: str) -> Optional[requests.Response]:
+    for _ in range(bcfg.max_retries):
+        try:
+            response = requests.get(url, timeout=bcfg.timeout)
             if response.status_code == 200:
-                gvar.wait_sec = max(cfg.min_wait, gvar.wait_sec * cfg.success_factor)  # decrease wait time on success
+                gvar.wait_sec = max(bcfg.min_wait, gvar.wait_sec * bcfg.success_factor)  # decrease wait time on success
                 return response
             else:
                 print(f"\n\tError {response.status_code}.")
                 print(f"\tRetrying in {gvar.wait_sec:.2f} seconds... ", end="", flush=True)
         except requests.Timeout:
-            print(f"\n\tRequest timed out after {cfg.timeout} seconds.")
+            print(f"\n\tRequest timed out after {bcfg.timeout} seconds.")
             print(f"\tRetrying in {gvar.wait_sec:.2f} seconds... ", end="", flush=True)
         except requests.RequestException as e:
             print(f"\n\t{e}.")
             print(f"\tRetrying in {gvar.wait_sec:.2f} seconds... ", end="", flush=True)
         time.sleep(gvar.wait_sec)
-        gvar.wait_sec = min(cfg.max_wait, gvar.wait_sec * cfg.fail_factor)  # increase wait time on failure
+        gvar.wait_sec = min(bcfg.max_wait, gvar.wait_sec * bcfg.fail_factor)  # increase wait time on failure
     print("\n\tMax retries reached.")
     return None
 def lnglat_to_tile_coords(lng: float, lat: float, z: int) -> Tuple[int, int]:
@@ -106,10 +148,10 @@ def lnglat_to_tile_coords(lng: float, lat: float, z: int) -> Tuple[int, int]:
     x = int(n * lon_n)
     y = int(n * lat_n)
     return x, y
-def get_tile_coords_list(tile_range: TileRange, zoom: int) -> List[Tuple[int, int]]:
-    minlon, minlat, maxlon, maxlat = tile_range
+def get_tile_coords_list(bounds: TileBounds, zoom: int) -> List[Tuple[int, int]]:
+    minlon, minlat, maxlon, maxlat = bounds
     tile_side_count: int = 2 ** zoom
-    if tile_range == MAX_RANGE:
+    if bounds == MAX_BOUNDS:
         return [(x, y) for x in range(tile_side_count) for y in range(tile_side_count)]
     else:
         x1, y1 = lnglat_to_tile_coords(minlon, minlat, zoom)
@@ -121,21 +163,21 @@ def get_tile_coords_list(tile_range: TileRange, zoom: int) -> List[Tuple[int, in
             return []
         print(f"Tile coordinates: ({minx}, {miny}) to ({maxx}, {maxy})")
         return [(x, y) for x in range(minx, maxx + 1) for y in range(miny, maxy + 1)]
-def download_one_tile(gvar: GlobalVariables, args: TileDLArguments, cfg: BackoffConfig, x: int, y: int) -> int:
+def download_one_tile(gvar: GlobalVariables, args: TileDLArguments, bcfg: BackoffConfig, x: int, y: int) -> int:
     tile_dir: str = os.path.join(args.dir, str(args.zoom), str(x))
     tile_path: str = os.path.join(tile_dir, f"{y}.{args.option.ext}")
     if os.path.exists(tile_path):
         return 0 # tile already exists, will skip downloading
     url: str = URL_TEMPLATE.format(t=args.option.name, z=args.zoom, x=x, y=y, e=args.option.ext, k=args.key)
-    response: Optional[requests.Response] = get_response_dynamic_backoff(gvar, cfg, url)
+    response: Optional[requests.Response] = get_response_dynamic_backoff(gvar, bcfg, url)
     if response is not None:
         os.makedirs(tile_dir, exist_ok=True)
         with open(tile_path, "wb") as f:
             f.write(response.content)
         return response.status_code # return status code in case of success
     return -1 # failed to download after retries, tile not downloaded
-def download_tiles(gvar: GlobalVariables, args: TileDLArguments, cfg: BackoffConfig) -> None:
-    tile_coords: List[Tuple[int, int]] = get_tile_coords_list(args.range, args.zoom)
+def download_tiles(gvar: GlobalVariables, args: TileDLArguments, bcfg: BackoffConfig) -> None:
+    tile_coords: List[Tuple[int, int]] = get_tile_coords_list(args.bounds, args.zoom)
     len_tiles = len(tile_coords) # number of tiles to download
     if len_tiles == 0:
         print("No tiles to download.")
@@ -147,7 +189,7 @@ def download_tiles(gvar: GlobalVariables, args: TileDLArguments, cfg: BackoffCon
         tile_progress = f"{i + 1:>{len_digits}}/{len_tiles:>{len_digits}}"
         tile_coords_progress = f"({x:>{side_digits}}, {y:>{side_digits}})"
         print(f"\033[2K\tDownloading {tile_progress}: {tile_coords_progress}... ", end="", flush=True)
-        status_code = download_one_tile(gvar, args, cfg, x, y)
+        status_code = download_one_tile(gvar, args, bcfg, x, y)
         if status_code == 0: # tile already exists, skip it
             print(f"SKP", end="\r")
             continue
@@ -164,10 +206,42 @@ if __name__ == "__main__":
     if not os.path.exists(args.dir):
         print(f"Directory {args.dir} does not exist. Creating it...")
         os.makedirs(args.dir)
-    cfg: BackoffConfig = BackoffConfig()
+    if not os.access(args.dir, os.W_OK):
+        print(f"Directory {args.dir} is not writable. Exiting.")
+        exit(1)
+    level_configs: List[LevelConfig] = []
+    bcfg: BackoffConfig = BackoffConfig()
     gvars: GlobalVariables = GlobalVariables()
-    try:
-        download_tiles(gvars, args, cfg)
-        print("Done.")
-    except KeyboardInterrupt:
-        print(f"\n\t...{gvars.downloaded_count} new tiles downloaded.\nInterrupted by user.")
+    if args.config != "":
+        if not os.path.exists(args.config):
+            print(f"Configuration file {args.config} does not exist. Exiting.")
+            exit(1)
+        if not os.access(args.config, os.R_OK):
+            print(f"Configuration file {args.config} is not readable. Exiting.")
+            exit(1)
+        print(f"Configuration file {args.config} is provided. Loading it...")
+        level_configs = load_config(args.config)
+        if not level_configs or len(level_configs) == 0:
+            print("No valid level configurations found. Exiting.")
+            exit(1)
+        print(f"Loaded {len(level_configs)} level configurations from {args.config}.")
+    else:
+        level_configs = [LevelConfig(zoom=args.zoom, bounds=args.bounds)]
+    for level in level_configs:
+        current_arguments: TileDLArguments = TileDLArguments(
+            key=args.key,
+            dir=args.dir,
+            option=args.option,
+            zoom=level.zoom,
+            bounds=level.bounds,
+            config=args.config
+        )
+        print(f"Processing zoom level {level.zoom} with bounds {level.bounds}...")
+        try:
+            download_tiles(gvars, current_arguments, bcfg)
+            gvars.total_downloaded_count += gvars.downloaded_count
+            gvars.downloaded_count = 0 # reset for next level
+        except KeyboardInterrupt:
+            print(f"\n\t...{gvars.downloaded_count} new tiles downloaded.\nInterrupted by user.")
+            exit(0)
+    print(f"\nDone. Total new tiles downloaded: {gvars.total_downloaded_count}.")
